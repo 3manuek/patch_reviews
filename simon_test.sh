2b4d9ba90f7428f0f306517278974226c398c5e4
Emanuel <3manuek@esdebian.org>
2014-02-06 16:18:29 -0200

    Update simon_test.sh

--- simon_test.sh
+++ simon_test.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 LOG=/tmp/machine_simon_test
 PATCH_LOG=/tmp/_patch
 NO_PATCH_LOG=/tmp/_nopatch
-LOOP=20
+LOOP=2
 CONN=60
 TXSS=100
 
 cat /dev/null > $LOG
 
 echo "Start"
-#echo "stats NOPATCH" >> $LOG
-#pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -x -c 'select blks_read , blks_hit from pg_stat_database where datname = $$postgres$$ ' >> $LOG
-#echo "stats PATCHED" >> $LOG
-#pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -x -c 'select blks_read , blks_hit from pg_stat_database where datname = $$postgres$$ ' >> $LOG
+pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -x -c 'select * from pg_settings where name = $$prune_page_dirty_limit$$' >> $LOG
+
+pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -c 'select pg_stat_reset(); select pg_stat_get_db_stat_reset_time(12913)' >> $LOG
+pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -c 'select pg_stat_reset(); select pg_stat_get_db_stat_reset_time(12913)' >> $LOG
 
 I_NOP_READ=$(pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -At -c 'select blks_read from pg_stat_database where datname = $$postgres$$')
 I_NOP_HIT=$(pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -At -c 'select blks_hit from pg_stat_database where datname = $$postgres$$')
@@ -27,35 +26,22 @@ echo $I_NOP_HIT
 echo $I_PAT_READ
 echo $I_PAT_HIT
 
-pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -x -c 'select * from pg_settings where name = $$prune_page_dirty_limit$$' >> $LOG
-
-pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -c 'select pg_stat_reset(); select pg_stat_get_db_stat_reset_time(12913)' >> $LOG
-pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -c 'select pg_stat_reset(); select pg_stat_get_db_stat_reset_time(12913)' >> $LOG
-
-
 echo "PGBENCH start"
 echo "PGBENCH TESTS" >> $LOG
 echo "" >>$LOG
 echo "WITH PATCH" >>$LOG
 
 for i in $(seq 1 $LOOP) ; do pg_SIMON_PATCH/bin/pgbench -c$CONN -t$TXSS -Sn -Upostgres -p7777 ; done | grep tps > $PATCH_LOG
-
 cat $PATCH_LOG | awk ' {sum+=$3} END {print "Average: ",sum/NR} ' >> $LOG
 
 echo "" >>$LOG
 echo "NO PATCH" >>$LOG
-
 for i in $(seq 1 $LOOP) ; do pg_NO_SIMON_PATCH/bin/pgbench -c$CONN -t$TXSS -Sn -Upostgres -p8888 ; done | grep tps > $NO_PATCH_LOG
 
 cat $NO_PATCH_LOG | awk ' {sum+=$3} END {print "Average: ",sum/NR} ' >> $LOG
 
 echo "Report"
 echo "REPORT" >> $LOG
-
-#echo "stats NOPATCH" >> $LOG
-#pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -x -c 'select blks_read , blks_hit from pg_stat_database where datname = $$postgres$$ ' >> $LOG
-#echo "stats PATCHED" >> $LOG
-#pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -x -c 'select blks_read , blks_hit from pg_stat_database where datname = $$postgres$$ ' >> $LOG
 
 L_NOP_READ=$(pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -At -c 'select blks_read from pg_stat_database where datname = $$postgres$$')
 L_NOP_HIT=$(pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -At -c 'select blks_hit from pg_stat_database where datname = $$postgres$$')
@@ -64,20 +50,19 @@ L_PAT_READ=$(pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -At -c 'select blks_read 
 L_PAT_HIT=$(pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -At -c 'select blks_hit from pg_stat_database where datname = $$postgres$$')
 
 F_NOP_READ=$((L_NOP_READ-I_NOP_READ))
-F_NOP_HIT=$((L_NOP_HIT-I_NOP_HIT))
+F_NOP_HIT=$(( L_NOP_HIT- I_NOP_HIT))
 F_PAT_READ=$((L_PAT_READ-I_PAT_READ))
-F_PAT_HIT=$((L_PAT_HIT-I_PAT_HIT))
-
+F_PAT_HIT=$(( L_PAT_HIT- I_PAT_HIT))
 
 echo "DEBUG"
 echo $I_NOP_READ
 echo $I_NOP_HIT
 echo $I_PAT_READ
 echo $I_PAT_HIT
-echo $F_NOP_READ
-echo $F_NOP_HIT
-echo $F_PAT_READ
-echo $F_PAT_HIT
+echo $L_NOP_READ
+echo $L_NOP_HIT
+echo $L_PAT_READ
+echo $L_PAT_HIT
 
 echo "RESULTS DIFFERENCES" >> $LOG
 
@@ -85,6 +70,3 @@ echo "F_NOP_READ " $F_NOP_READ >> $LOG
 echo "F_NOP_HIT  " $F_NOP_HIT >> $LOG
 echo "F_PAT_READ " $F_PAT_READ >> $LOG
 echo "F_PAT_HIT  " $F_PAT_HIT >> $LOG
-
-#pg_NO_SIMON_PATCH/bin/psql -p8888 -Upostgres -c 'select * from pg_stat_database' >> $LOG
-#pg_SIMON_PATCH/bin/psql -p7777 -Upostgres -c 'select * from pg_stat_database' >> $LOG
